8058f82626464707a5e3ab73bdf5d784e9158162
Geoff Evans <geoff@flummoxed.org>
2020-10-04 14:06:10 -0700

    remove win32 and add github action

--- build_wx.bat
+++ build_wx.bat
@@ -1,11 +1,13 @@
 @echo off
 
 setlocal
-pushd
 
-cd %~dp0\submodule\wxWidgets\build\msw
-cmd.exe /c "call "%VCINSTALLDIR%"\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=debug %*"
-cmd.exe /c "call "%VCINSTALLDIR%"\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=release %*"
+for /f "delims=" %%f in ( '"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath' ) do set VSWHERE=%%f
+echo VSWHERE=%VSWHERE%
+
+pushd %~dp0\submodule\wxWidgets\build\msw
+cmd.exe /c "call "%VSWHERE%"\VC\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=debug %*"
+cmd.exe /c "call "%VSWHERE%"\VC\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=release %*"
 
 endlocal
 popd
