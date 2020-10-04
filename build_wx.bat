0ea506cc771b9b80516208a847ac90b92e554456
Geoff Evans <geoff@flummoxed.org>
2020-10-04 14:31:54 -0700

    update github action

--- build_wx.bat
+++ build_wx.bat
@@ -6,8 +6,14 @@ for /f "delims=" %%f in ( '"C:\Program Files (x86)\Microsoft Visual Studio\Insta
 echo VSWHERE=%VSWHERE%
 
 pushd %~dp0\submodule\wxWidgets\build\msw
-cmd.exe /c "call "%VSWHERE%"\VC\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=debug %*"
-cmd.exe /c "call "%VSWHERE%"\VC\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=release %*"
+
+if "%1" equ "" set BUILD_DEBUG=1
+if "%1" equ "" set BUILD_RELEASE=1
+if "%1" equ "debug" set BUILD_DEBUG=1
+if "%1" equ "release" set BUILD_RELEASE=1
+
+if defined BUILD_DEBUG cmd.exe /c "call "%VSWHERE%"\VC\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=debug"
+if defined BUILD_RELEASE cmd.exe /c "call "%VSWHERE%"\VC\Auxiliary\Build\vcvars64.bat && nmake.exe /f makefile.vc TARGET_CPU=AMD64 RUNTIME_LIBS=static DEBUG_INFO=1 BUILD=release"
 
 endlocal
 popd
