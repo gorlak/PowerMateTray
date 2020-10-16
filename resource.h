5c86b7cb6cb36eac1f406245701b415115996cab
Geoff Evans <geoff@flummoxed.org>
2020-10-16 10:05:09 -0700

    1.0.1 draft

--- resource.h
+++ resource.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR			1
 #define VERSION_MINOR			0
-#define VERSION_PATCH			0
+#define VERSION_PATCH			1
 
 #define VERSION_STRINGIFY(major, minor, patch) wxT(#major) wxT(".") wxT(#minor) wxT(".") wxT(#patch)
 #define VERSION_TOSTRING(major, minor, patch) VERSION_STRINGIFY(major,minor,patch)
